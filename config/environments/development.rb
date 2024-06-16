0c4ac6093b89ab127f37ff893236138a0ca81224
Danylo Bondarenko <bondanil01@gmail.com>
2024-06-16 14:03:47 +0300

    Add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.hosts << /[a-z0-9\-]+\.ngrok-free\.app/
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
