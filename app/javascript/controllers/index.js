807b73323a67705d66566dffc0bcf8b736748e3f
Danylo Bondarenko <bondanil01@gmail.com>
2024-11-24 15:06:08 +0200

    Use stimulus component for clipboard

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,6 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application"
-
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
@@ -12,3 +8,6 @@ eagerLoadControllersFrom("controllers", application)
 
 import  ImageSelectorController from "./image_selector_controller"
 application.register("image-selector", ImageSelectorController)
+
+import Clipboard from '@stimulus-components/clipboard'
+application.register('clipboard', Clipboard)
